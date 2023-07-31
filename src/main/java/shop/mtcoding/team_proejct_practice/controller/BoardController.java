9385f804b825ad098898fcd82201f4de2f7e7865
jonggwooo <kimwj44153@naver.com>
2023-07-31 14:40:36 +0900

    컨트롤러 기능구현 완료

--- src/main/java/shop/mtcoding/team_proejct_practice/controller/BoardController.java
+++ src/main/java/shop/mtcoding/team_proejct_practice/controller/BoardController.java
@@ -1,5 +1,16 @@
 package shop.mtcoding.team_proejct_practice.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BoardController {
+
+    @GetMapping({"/","/board"})
+    public String index(){
+        return "index";
+    }
+
+
 
 }
