76b6d1ad3969192d588c170511dd78f3b940cbe2
muaga <more9494@naver.com>
2023-08-23 13:59:12 +0900

    updateDTO 완료

--- src/main/java/shop/mtcoding/team_proejct_practice/dto/UpdateDTO.java
+++ src/main/java/shop/mtcoding/team_proejct_practice/dto/UpdateDTO.java
@@ -1,5 +1,11 @@
 package shop.mtcoding.team_proejct_practice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UpdateDTO {
+    private String username;
     private String password;
 }
